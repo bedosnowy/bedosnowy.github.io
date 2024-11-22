de07ec6f7673ef0ecc4082cec8eb9a80641fce39
bedosnowy <bedocherry35@gmail.com>
2024-11-22 09:54:04 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const messages = [
+        "Welcome! How are you?",
+        "Check out our discord server!",
+        "Bedosnowy always",
+        "giggity giggity giggity giggity!",
+        "The source code of the site is available on my github!"
+    ];
+
+    const updateElement = document.getElementById("updates");
+    let currentIndex = 0;
+    let isDeleting = false;
+    let charIndex = 0;
+    const typingSpeed = 20;
+    const deletingSpeed = 20;
+    const pauseTime = 2000;
+    const deletePauseTime = 1000;
+
+    function updateMessage() {
+        const currentMessage = messages[currentIndex];
+
+        if (isDeleting) {
+            charIndex--;
+            updateElement.innerHTML = `<span style="color: #3465A4;">~</span> <span style="color: #4E9A06;">$</span> ${currentMessage.substring(0, charIndex)}`;
+        } else {
+            charIndex++;
+            updateElement.innerHTML = `<span style="color: #3465A4;">~</span> <span style="color: #4E9A06;">$</span> ${currentMessage.substring(0, charIndex)}`;
+        }
+
+        if (!isDeleting && charIndex === currentMessage.length) {
+            isDeleting = true;
+            setTimeout(updateMessage, pauseTime);
+            return;
+        } else if (isDeleting && charIndex === 0) {
+            isDeleting = false;
+            currentIndex = (currentIndex + 1) % messages.length;
+            setTimeout(updateMessage, deletePauseTime);
+            return;
+        }
+
+        const speed = isDeleting ? deletingSpeed : typingSpeed;
+        setTimeout(updateMessage, speed);
+    }
+
+    updateMessage();
+});
